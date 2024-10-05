70775d843cd2ee65e66e7679245de3b46d5f6f11
Vishalsahani156 <vishalsahani1444@gmail.com>
2024-10-06 00:46:42 +0530

    complite the recursion code

--- Recursion16.cpp
+++ Recursion16.cpp
@@ -1,19 +1,88 @@
 #include<iostream>
 #include<iomanip>
 using namespace std;
-void sumOfFirstNatureNumber(int number){
-    int sum=0;
-    if(number>=0){
-        sumOfFirstNatureNumber(number-1);
-        sum=+number;
+void sumOfFirstNatureNumber(int number,int &sum){
+    int x=number;
+    if(x>=0){
+        sumOfFirstNatureNumber(x-1,sum);
+        sum=sum+x;
         cout<<sum<<endl;
     }
 }
-int main(){
-    cout<<"enter the number to print the sum of number "<<endl;
-    int number;
-    cin>>number;
-    sumOfFirstNatureNumber(number);
+void oddNatureNumber(int number,int &sum){
+    if(number >=1){
+        oddNatureNumber(number-1,sum);
+    //   sum=sum+(number*2); // this is problem in the code
+         sum=2*number-1;
+         cout<<(sum)<<endl;
+    }
+}
+void evenNumber(int number,int &sum){
+    if(number==0){
+        evenNumber(number-1,sum);
+        sum=sum+(number*2);
+        cout<<sum<<endl;
+    }
+}
+void factorialCalculate(int number,int &sum){
+    if(number<=1)
+    return;
+factorialCalculate(number-1,sum);
+  sum=sum*number;
+  cout<<sum<<endl;
+}
+int sumOfDigit(int number){
+    if(number==0)
+    return 0;
+return (number%10)+sumOfDigit(number/10);
 
+}
+void binaryToDecimal(int number){
+    if(number >0){
+binaryToDecimal(number/2);
+printf("%d",number %2);
+    }
+}
+
+int fibNac(int number){
+    if(number==0)
+    return 0;
+    if(number==1)
+        return 1;
+    return fibNac(number-1)+ fibNac(number-2);
+}
+int calHcf(int n1,int n2){
+    if(n1>n2)
+    if(n1%n2==0)
+    return  n2;
+else
+return calHcf(n1%n2,n2);
+else
+if(n2%n1==0)
+return n1;
+else
+return calHcf(n2,n2%n1);
+}
+void calPower(int number){
+
+}
+int main(){
+    // cout<<"enter the number to print the sum of number "<<endl;
+    // int number=5;
+    // cin>>number;
+    // int sum=1;
+    // sumOfFirstNatureNumber(number,sum);
+    // oddNatureNumber(number,sum);
+    // evenNumber(number,sum);
+    // factorialCalculate(number,sum);
+    // int number=12345;
+    // cout<< sumOfDigit(number)<<endl;
+    int number=10;
+    binaryToDecimal(number);
+    // int number=10;
+//    cout<<fibNac(number)<<endl;
+   int n1=4;
+   int n2=5;
+// cout<<calHcf(n1,n2)<<endl;
     return 0;
 }
--- Test.cpp
+++ Test.cpp
@@ -1,19 +1,31 @@
 #include<iostream>
 #include<iomanip>
 using namespace std;
-void printCubes(int cubes,int x){
+int fib1(int number){
+  if(number==0)
+  return 0;
+  if(number==1)
+  return 1;
 
-    if(cubes==0)
-    return ;
-   printCubes (cubes-1);
-cout<<cubes*cubes*cubes<<endl;
-// cout<<x<<endl;
-
+  return fib1(number-1)+ fib1(number-2);
 }
-
+void odd(int number,int &sum){
+  if(number==0)
+  return ;
+    odd(number-1,sum);
+    sum=2*number-1;
+     cout<<sum<<endl;
+}
+void binaryToDecimal(int number){
+  binaryToDecimal(number/2);
+  cout<<number%2<<endl;
+}
 int main(){
- int cubes=10;
-  printCubes(cubes,x);
+  int number=10;
+  // cout<<fib1(number)<<endl;
+  int sum=0;
+  // odd(number,sum);
+  binaryToDecimal(number);
  
-    return 0;
+  return 0;
 }
